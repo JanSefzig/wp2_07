47dde18be576e1525411afa90f2139a3c83099c6
JanSefzig <nicolas.jan.sefzig@student.ossp.cz>
2021-12-17 09:57:10 +0100

    com

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+require_once('functions2.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $people[0]= 32;
+        $people[1]= 17;    
+        $people[2]= 13;
+        $people[3]= 99;
+
+        for($i=0; $i< count($people); $i++){
+            myDummyFunction($people[$i]);
+        }
+    ?>
+</body>
+</html>
